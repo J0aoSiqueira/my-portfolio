24d933d551c3f2cffc801273bb1155d8c7c167c9
João Ricardo <joao.pradosiqueira@gmail.com>
2024-10-22 19:31:17 -0300

    Removed Grid import

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
-import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
