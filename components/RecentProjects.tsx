ffcaf27a87773b0602bd0b673bf6ce3172dcd190
João Ricardo <joao.pradosiqueira@gmail.com>
2024-10-23 12:25:16 -0300

    Added RecentProjects sections, adding content soon

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const RecentProjects = () => {
   return (
-    <div>RecentProjects</div>
+    <div className="py-20">
+      <h1 className="heading">
+        Em breve...
+      </h1>
+    </div>
   )
 }
 
