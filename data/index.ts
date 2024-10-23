6d112aebace20c488cb63bd81b8a9a81e74bd0fc
João Ricardo <joao.pradosiqueira@gmail.com>
2024-10-23 19:24:06 -0300

    Added technologies data for the technologies section

--- data/index.ts
+++ data/index.ts
@@ -22,3 +22,70 @@ export const socialMedia = [
     link:"https://www.linkedin.com/in/joao-p-siqueira/",
   },
 ];
+
+export const technologies = [
+  {
+    id: 1,
+    img: "/nextjs-fill-svgrepo-com.svg",
+    name: "NextJS", 
+    
+  },
+  {
+    id: 2,
+    img: "/react-svgrepo-com.svg",
+    name: "React",
+  },
+  {
+    id: 3,
+    img: "/typescript-icon-svgrepo-com.svg",
+    name: "Typescript",
+  },
+  {
+    id: 4,
+    img: "/cpp-svgrepo-com.svg",
+    name: "C++",
+
+  },
+  {
+    id: 5,
+    img: "/csharp-svgrepo-com.svg",
+    name: "C#",
+
+  },
+  {
+    id: 6,
+    img: "/python-svgrepo-com.svg",
+    name: "Python",
+  },
+  {
+    id: 7,
+    img: "/html-5-svgrepo-com.svg",
+    name: "HTML",
+  },
+  {
+    id: 8,
+    img: "/css-3-svgrepo-com.svg",
+    name: "CSS",
+  },
+  {
+    id: 9,
+    img: "/js-svgrepo-com.svg",
+    name: "Javascript",
+
+  },
+  {
+    id: 10,
+    img: "/postgresql-svgrepo-com.svg",
+    name: "PostgreSQL",
+  },
+  {
+    id: 11,
+    img: "/arch-linux-svgrepo-com.svg",
+    name: "Arch Linux",
+  },
+  {
+    id: 12,
+    img: "/tailwind-svgrepo-com.svg",
+    name: "TailwindCSS",
+  },
+];
