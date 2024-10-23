fb5a2731a8d76b96f37b5d8b8469733a0a86f85a
João Ricardo <joao.pradosiqueira@gmail.com>
2024-10-23 19:25:38 -0300

    Added grid elements for technology section, each one is a technology that I'm currently using

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -1,8 +1,23 @@
+import { technologies } from '@/data'
 import React from 'react'
 
 const Technologies = () => {
   return (
-    <div>Technologies</div>
+    <div className="flex flex-col items-center justify-center">
+      <div>
+        <h1 className="heading">
+          <span className="text-pink-400">Tecnologias</span> que utilizo atualmente
+        </h1>
+      </div>
+      <div className="grid grid-cols-6 gap-5 items-center justify-center my-10">
+        {technologies.map((tech) => (
+                    <div key={tech.id} className={"p-5 grid justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"}>
+                            <img src={tech.img} alt="icons" width={50} height={50} className="items-center"/>
+                            <p className="text-center">{tech.name}</p>
+                    </div> 
+                ))}
+      </div> 
+    </div>
   )
 }
 
