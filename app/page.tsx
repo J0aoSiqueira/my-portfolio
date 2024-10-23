daa547ad1ce544e3efed077443c9583d92d1bc35
João Ricardo <joao.pradosiqueira@gmail.com>
2024-10-23 14:12:10 -0300

    Added About Me, Technologies, Recent Projects and Footer sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,19 @@ import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 import RecentProjects from "@/components/RecentProjects";
+import Footer from "@/components/Footer";
+import AboutMe from "@/components/AboutMe";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome />}
-        ]}/>
+        <FloatingNav navItems={navItems}/>
         <Hero />
+        <AboutMe />
         <RecentProjects />
+        <Footer />
       </div>
     </main>
   );
