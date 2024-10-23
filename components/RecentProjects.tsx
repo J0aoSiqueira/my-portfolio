a733486ee360269fc9c9b059349fecea8b3b3fcc
João Ricardo <joao.pradosiqueira@gmail.com>
2024-10-23 16:04:11 -0300

    Fixed text resolution

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const RecentProjects = () => {
   return (
-    <div className="py-20" id="projetos">
-      <h1 className="flex flex-col text-center heading">
+    <div className="flex flex-col py-20 items-center justify-center h-screen" id="projetos">
+      <h1 className="heading">
         Meus <span className="text-pink-400">projetos</span> recentes
       </h1>
       <p className="text-white-200 md:mt-10 my-5 text-lg lg:text-2xl sm:px-16 xl:px-48">Em breve...</p>
