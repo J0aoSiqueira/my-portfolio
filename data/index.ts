20f24abf9bfe79cb9fd18958e20955befd265fd2
João Ricardo <joao.pradosiqueira@gmail.com>
2024-10-24 13:00:20 -0300

    Added home and projects links to the navItens; Added Whatsapp for social media

--- data/index.ts
+++ data/index.ts
@@ -1,23 +1,29 @@
 export const navItems = [
+  { name: "Início", link: "#home"},
   { name: "Sobre Mim", link: "#about" },
   { name: "Tecnologias", link: "#technologies" },
-  { name: "Projetos", link: "#projetos" },
+  { name: "Projetos", link: "#projects" },
   { name: "Contato", link: "#contact" },
 ];
 
 export const socialMedia = [
   {
     id: 1,
+    img: "/whatsapp-white-icon.svg",
+    link: "https://api.whatsapp.com/send?phone=5518996953530",
+  },
+  {
+    id: 2,
     img: "/git.svg",
     link:"https://github.com/J0aoSiqueira/",
   },
   {
-    id: 2,
+    id: 3,
     img: "/insta.svg",
     link:"https://www.instagram.com/joaoricardo_ps/", 
   },
   {
-    id: 3,
+    id: 4,
     img: "/link.svg",
     link:"https://www.linkedin.com/in/joao-p-siqueira/",
   },
