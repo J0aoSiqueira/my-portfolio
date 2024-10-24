9796f3e2faf5b041dce0c2ad7ef54fc3b16ecbd0
João Ricardo <joao.pradosiqueira@gmail.com>
2024-10-24 12:39:25 -0300

    Changed a different color for Spotlight; Added a redirect on Navigation bar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,10 +6,10 @@ import { FaAddressBook } from 'react-icons/fa6'
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36'>
+    <div className='pb-20 pt-36' id="#home">
         <div>
-            <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
-            <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
+            <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="#00ffff" />
+            <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="blue" />
             <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="pink" />
 
         </div>
