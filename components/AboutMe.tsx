150cc2d4371f53e43e8a796e2b66ea049896cd0a
João Ricardo <joao.pradosiqueira@gmail.com>
2024-10-24 14:55:13 -0300

    text justified on AboutMe section

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
           <h1 className="heading">
             Sobre <span className="text-pink-400">mim</span>
           </h1>
-            <p className="text-[20px] mt-5 pl-50">
+            <p className="text-[20px] mt-5 pl-50 text-justify">
               Meu nome é João Ricardo, um desenvolvedor de software, tenho 20 anos e atualmente sou graduando em Bacharelado em Ciência da Computação na Universidade Tecnológica Federal do Paraná (UTFPR-CM).
               Busco expandir minha experiência profissional nas diferentes áreas da tecnologia e em desenvolvimento de software, além de estar disposto a aprender e ganhar experiência e networking na carreira profisional.
               Tenho proeficiência B1+ em inglês, além da proeficiência básica em espanhol e alemão, os quais sou capaz de falar e compreender os idiomas. Atualmente, utilizo as tecnologias NextJS, React e TypeScript tanto no ramo acadêmico e profissional. Sou uma pessoa que valoriza a qualidade e a organização no trabalho.
