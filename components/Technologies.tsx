f349c6c4f575e2c5769fc234fd8cd57822d573f4
João Ricardo <joao.pradosiqueira@gmail.com>
2024-10-24 12:59:26 -0300

    Added responsive for mobile devices on seeing the technologies section

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -6,14 +6,14 @@ const Technologies = () => {
     <div className="flex flex-col items-center justify-center" id="technologies">
       <div>
         <h1 className="heading">
-          <span className="text-pink-400">Tecnologias</span> que utilizo atualmente
+          <span className="text-pink-400">Tecnologias</span> que utilizo atualmente <br/> ou utilizei anteriormente 
         </h1>
       </div>
-      <div className="grid grid-cols-6 gap-5 items-center justify-center my-10">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-5 items-center justify-center my-10">
         {technologies.map((tech) => (
-                    <div key={tech.id} className={"p-5 grid justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"}>
-                            <img src={tech.img} alt="icons" width={50} height={50} className="items-center"/>
-                            <p className="text-center">{tech.name}</p>
+                    <div key={tech.id} className={"p-5 grid justify-center text-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"}>
+                            <img src={tech.img} alt="icons" width={50} height={50} className="mx-auto"/>
+                            <p className="mt-2">{tech.name}</p>
                     </div> 
                 ))}
       </div> 
